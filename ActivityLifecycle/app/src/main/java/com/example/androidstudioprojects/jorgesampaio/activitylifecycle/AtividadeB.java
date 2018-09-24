2e6e6d2bdb347fb4fe9355e2b22058eefc1d542b
usjt2016 <jorgelimass@hotmail.com>
2018-09-24 19:08:35 -0300

    Exercicios

---
+++ ActivityLifecycle/app/src/main/java/com/example/androidstudioprojects/jorgesampaio/activitylifecycle/AtividadeB.java
@@ -0,0 +1,88 @@
+package com.example.jorgesampaio.androidstudioprojects.activitylifecycle;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AtividadeB extends AppCompatActivity {
+
+    private EditText atividadeB;
+    private EditText statusAtividadeB;
+    private String dadosatividadeA;
+    private EditText dadosAtividadeB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atividade_b);
+        Log.i("ciclodevida", "AtividadeB:onCreate");
+
+        dadosatividadeA = getIntent().getStringExtra("atividadesA");
+
+        dadosAtividadeB = findViewById(R.id.atividadeB);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ciclodevida", "AtividadeB:onStart");
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ciclodevida", "AtividadeB:onResume");
+        String status = ("Atividade B: Resumed \n Atividade A:Paused");
+        statusAtividadeB = (EditText) findViewById(R.id.statusAtividadeB);
+        statusAtividadeB.setText(status);
+
+        String mensagem = (" B.onResume()\n B.onStart()\n B.onCreate \n");
+        atividadeB = (EditText) findViewById(R.id.atividadeB);
+        atividadeB.setText(mensagem + dadosatividadeA);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ciclodevida", "AtividadeB:onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ciclodevida", "AtividadeB:onPause");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ciclodevida", "AtividadeB:onDestroy");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ciclodevida", "AtividadeB:onRestart");
+    }
+
+
+    public void iniciarAtividade_BC(View view) {
+        Log.i("ciclodevida", "AtividadeB:entrou nesse metodo");
+
+        String dadosAtividades = dadosAtividadeB.getEditableText().toString();
+
+        Intent intent = new Intent(this, AtividadeC.class);
+        intent.putExtra("atividadesB", dadosAtividades);
+        startActivity(intent);
+    }
+}
---
+++ ActivityLifecycle/app/src/main/java/com/example/androidstudioprojects/jorgesampaio/activitylifecycle/AtividadeC.java
@@ -0,0 +1,63 @@
+package com.example.jorgesampaio.androidstudioprojects.activitylifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+public class AtividadeC extends AppCompatActivity {
+    private String dadosatividadeB;
+    private EditText atividadeC;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atividade_c);
+        Log.i("ciclodevida","AtividadeC:onCreate");
+        dadosatividadeB = getIntent().getStringExtra("atividadesB");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ciclodevida","AtividadeC:onStart");
+
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ciclodevida","AtividadeC:onResume");
+
+        String mensagem = (" B.onPause \n A.onStop \n C.onResume()\n C.onStart()\n C.onCreate \n");
+        atividadeC = (EditText) findViewById(R.id.atividadeC);
+        atividadeC.setText(mensagem + dadosatividadeB);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ciclodevida","AtividadeC:onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ciclodevida","AtividadeC:onPause");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ciclodevida","AtividadeC:onDestroy");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ciclodevida","AtividadeC:onRestart");
+    }
+
+}
