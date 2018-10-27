fd0994fd965a8353cd569ce10e74422eed7c5cc1
usjt2016 <jorgelimass@hotmail.com>
2018-10-27 10:40:54 -0300

    A1

---
+++ A1/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jorgesampaio.helloworld;
+/*Jorge sampaio RA: 81513901*/
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
